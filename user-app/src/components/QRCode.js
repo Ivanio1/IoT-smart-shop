03dcfecce4d22ee053eb9d4d90f7f735d4a2672e
Ivanio1 <sobolev1van@yandex.ru>
2024-03-12 15:43:21 +0300

    feat: init qr-code resolver

--- user-app/src/components/QRCode.js
+++ user-app/src/components/QRCode.js
@@ -1,6 +1,7 @@
 import {Html5Qrcode} from "html5-qrcode";
 import React, {useState, useEffect} from "react";
 import Swal from 'sweetalert2';
+import {DEFAULT_URL} from "../index";
 
 window.Swal = Swal;
 
@@ -9,62 +10,71 @@ function QRCode() {
     const [qrMessage, setQrMessage] = useState("");
 
     useEffect(() => {
-        const config = {fps: 10, qrbox: {width: 200, height: 200}};
+            const config = {fps: 10, qrbox: {width: 200, height: 200}};
 
-        const html5QrCode = new Html5Qrcode("qrCodeContainer");
+            const html5QrCode = new Html5Qrcode("qrCodeContainer");
 
-        const qrScanerStop = () => {
-            if (html5QrCode && html5QrCode.isScanning) {
-                html5QrCode
-                    .stop()
-                    .then((ignore) => console.log("Scaner stop"))
-                    .catch((err) => console.log("Scaner error"));
+            const qrScanerStop = () => {
+                if (html5QrCode && html5QrCode.isScanning) {
+                    html5QrCode
+                        .stop()
+                        .then((ignore) => console.log("Scaner stop"))
+                        .catch((err) => console.log("Scaner error"));
+                }
+            };
+
+            const qrCodeSuccess = (decodedText) => {
+                setQrMessage(decodedText);
+                Swal.fire({
+                    title: "Товар успешно добавлен в корзину!",
+                    text: "Продолжайте свои покупки",
+                    icon: "success",
+                    timer: 2000,
+                    showCancelButton: false,
+                    showConfirmButton: false
+                }).then(
+                    setEnabled(true)
+                )
+
+                //Отправить запрос
+                let req = new XMLHttpRequest();
+                req.open("GET", `${DEFAULT_URL}/products?code=${qrMessage}`, true);
+                req.onload = () => handleResponse(req.responseText);
+                req.onerror = () => alert("Сервер временно недоступен");
+                req.setRequestHeader('Content-Type', 'application/json');
+                req.send();
+
+                setEnabled(false)
+            };
+
+            const handleResponse = (text) => {
+                    let response = JSON.parse(text);
+                    if (response.status === 200) {
+                       alert(response.answer)
+                    } else {
+                        alert("Error");
+                    }
+                }
+            ;
+
+            if (isEnabled) {
+                html5QrCode.start({facingMode: "environment"}, config, qrCodeSuccess);
+                setQrMessage("");
+            } else {
+                qrScanerStop();
             }
-        };
 
-        const qrCodeSuccess = (decodedText) => {
-            setQrMessage(decodedText);
-            Swal.fire({
-                title: "Товар успешно добавлен в корзину!",
-                text: "Продолжайте свои покупки",
-                icon: "success",
-                timer: 2000,
-                showCancelButton: false,
-                showConfirmButton: false
-            }).then(
-                setEnabled(true)
-            )
-
-            //Отправить запрос
-            // const request = new Request(qrMessage);
-            //
-            // fetch(request)
-            //     .then((response) => {
-            //         if (response.status === 200) {
-            //             return response.json();
-            //         } else {
-            //             throw new Error("Something went wrong on API server!");
-            //         }
-            //     });
-
-        };
-
-        if (isEnabled) {
-            html5QrCode.start({facingMode: "environment"}, config, qrCodeSuccess);
-            setQrMessage("");
-        } else {
-            qrScanerStop();
-        }
-
-        return () => {
-            qrScanerStop();
-        };
-    }, [isEnabled]);
+            return () => {
+                qrScanerStop();
+            };
+        }, [isEnabled]
+    )
+    ;
 
     return (
         <div className="scaner">
+            <div><h1>Отсканируйте qr-код необходимого товара, чтобы добавить его в корзину!</h1></div>
             <div id="qrCodeContainer"/>
-
             <br/><br/>
             <div>
                 <button className="start-button" onClick={() => setEnabled(!isEnabled)}>
