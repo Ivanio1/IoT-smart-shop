01a539d9ea40ad374195bc1bda16b285582d7e22
Ivanio1 <sobolev1van@yandex.ru>
2024-03-12 14:02:25 +0300

    feat: add qr code reader

---
+++ user-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="head">
+            <div>
+                <br/>
+                <span className="logo">Умный магазин</span>
+                <br/><br/>
+            </div>
+        </header>)
+}
---
+++ user-app/src/components/QRCode.js
@@ -0,0 +1,80 @@
+import {Html5Qrcode} from "html5-qrcode";
+import React, {useState, useEffect} from "react";
+import Swal from 'sweetalert2';
+
+window.Swal = Swal;
+
+function QRCode() {
+    const [isEnabled, setEnabled] = useState(false);
+    const [qrMessage, setQrMessage] = useState("");
+
+    useEffect(() => {
+        const config = {fps: 10, qrbox: {width: 200, height: 200}};
+
+        const html5QrCode = new Html5Qrcode("qrCodeContainer");
+
+        const qrScanerStop = () => {
+            if (html5QrCode && html5QrCode.isScanning) {
+                html5QrCode
+                    .stop()
+                    .then((ignore) => console.log("Scaner stop"))
+                    .catch((err) => console.log("Scaner error"));
+            }
+        };
+
+        const qrCodeSuccess = (decodedText) => {
+            setQrMessage(decodedText);
+            Swal.fire({
+                title: "Товар успешно добавлен в корзину!",
+                text: "Продолжайте свои покупки",
+                icon: "success",
+                timer: 2000,
+                showCancelButton: false,
+                showConfirmButton: false
+            }).then(
+                setEnabled(true)
+            )
+
+            //Отправить запрос
+            // const request = new Request(qrMessage);
+            //
+            // fetch(request)
+            //     .then((response) => {
+            //         if (response.status === 200) {
+            //             return response.json();
+            //         } else {
+            //             throw new Error("Something went wrong on API server!");
+            //         }
+            //     });
+
+        };
+
+        if (isEnabled) {
+            html5QrCode.start({facingMode: "environment"}, config, qrCodeSuccess);
+            setQrMessage("");
+        } else {
+            qrScanerStop();
+        }
+
+        return () => {
+            qrScanerStop();
+        };
+    }, [isEnabled]);
+
+    return (
+        <div className="scaner">
+            <div id="qrCodeContainer"/>
+
+            <br/><br/>
+            <div>
+                <button className="start-button" onClick={() => setEnabled(!isEnabled)}>
+                    {isEnabled ? " Закрыть сканирование " : "Сканировать qr-код"}
+                </button>
+            </div>
+        </div>
+    );
+
+
+}
+
+export default QRCode;
