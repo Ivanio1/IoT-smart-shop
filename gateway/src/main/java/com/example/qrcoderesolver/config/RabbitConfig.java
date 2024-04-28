417f23192eeba38906a8299ce69f02bb33ce2bcf
moyakkauntda <moyakkauntda@gmail.com>
2024-04-28 16:08:30 +0300

    lol

--- gateway/src/main/java/com/example/qrcoderesolver/config/RabbitConfig.java
+++ gateway/src/main/java/com/example/qrcoderesolver/config/RabbitConfig.java
@@ -1,14 +1,11 @@
 package com.example.qrcoderesolver.config;
 
-import javax.sound.midi.Receiver;
-
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
