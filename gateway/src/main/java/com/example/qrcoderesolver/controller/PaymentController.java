8fca856f354018edb64707f3fd6541886de89cba
moyakkauntda <moyakkauntda@gmail.com>
2024-04-30 11:59:28 +0300

    lol

--- gateway/src/main/java/com/example/qrcoderesolver/controller/PaymentController.java
+++ gateway/src/main/java/com/example/qrcoderesolver/controller/PaymentController.java
@@ -3,18 +3,24 @@ package com.example.qrcoderesolver.controller;
 
 import com.example.qrcoderesolver.model.Product;
 import com.example.qrcoderesolver.model.ResponseMessage;
+import com.example.qrcoderesolver.service.RabbitService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/pay")
 public class PaymentController {
+    @Autowired
+    private RabbitService rabbitService;
 
     @PostMapping()
     public ResponseMessage getProduct(@RequestParam String type, @RequestBody ArrayList<Product> orders) {
@@ -29,9 +35,7 @@ public class PaymentController {
                 int bytesRead = socket.getInputStream().read(buffer);
                 String response = new String(buffer, 0, bytesRead);
                 if (response.equals("OK")) {
-
-                    //запрос в кафку
-
+                    rabbitService.sendToTerminal(formatOrders(orders));
                     return new ResponseMessage(200, "Payment successful!");
                 } else return new ResponseMessage(400, "Payment error!");
 
@@ -40,4 +44,10 @@ public class PaymentController {
             }
         } else return new ResponseMessage(404, "Not found!");
     }
+
+    private String formatOrders(List<Product> orders) {
+        return orders.stream().map(product -> product.getTitle() + " " + product.getExpirationPoints())
+                .collect(Collectors.joining("\n"));
+    }
+
 }
